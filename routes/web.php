0e83a5fc08866c0e18fcba38ab5fbc4e7a7e32d0
manozghale <manozghale@gmail.com>
2026-06-16 23:52:56 +0545

    Simplify welcome view and set app title
    Remove large inlined Tailwind fallback and streamline the <head> of the welcome view to reduce file size and complexity; update the default app title to "Event Hub" and keep Blade font directives. Also include minor related updates to routes/web.php and tests/Feature/ExampleTest.php.

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,15 @@ use App\Http\Controllers\LabelController;
 use App\Http\Controllers\OnsiteRegistrationController;
 use App\Http\Controllers\PublicRegistrationController;
 use App\Http\Controllers\TicketController;
+use App\Models\Event;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $events = Event::where('status', 'published')
+        ->orderBy('start_datetime')
+        ->get();
+
+    return view('welcome', ['events' => $events]);
 });
 
 Route::get('/checkin/t/{token}', [CheckinController::class, 'show'])->name('checkin.verify');
